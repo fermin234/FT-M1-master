e4e53098559a602644e7d5ce2fcae28f441982a1
fermin234 <fermin234@hotmail.com>
2022-06-20 16:32:30 -0300

    Se realio la homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,29 @@
 'use strict'
-
+// num = '0010' --> 2
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  // combierte el string en un arreglo de numeros
+  num = num.split("");
+  
+  // combierte el arreglo de numeros en un numero decimal
+  let res = 0;
+  let exp = num.length - 1;
+  for(let i = 0; i < num.length; i++){
+ 		res += num[i] * (2**exp);
+    exp --;
+  }
+  return res;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let res = [];
+  while(num > 0){
+    res.unshift(num % 2);
+    num = parseInt(num / 2);
+  }
 
+  return res.join("");
 }
 
 
